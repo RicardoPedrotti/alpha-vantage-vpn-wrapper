2f9d7cc037be2e15421ad638c8ba2c866d7064b6
RicardoPedrotti <rpedrottivf@gmail.com>
2021-05-26 00:12:33 +0200

    Adding converters to all values

--- fenix_alpha_vantage_interface/alpha_stock_timeseries.py
+++ fenix_alpha_vantage_interface/alpha_stock_timeseries.py
@@ -21,9 +21,20 @@ class AlphaStockTimeSeries(AlphaData):
         adjusted: str = True,
         use_vpn: bool = False,
     ) -> pd.DataFrame:
+        # Endpoint Configs
         endpoint = "TIME_SERIES_INTRADAY"
         possible_intervals = ["1min", "5min", "15min", "30min", "60min"]
-        self.logging_info_start(endpoint=endpoint, intervals=possible_intervals, use_vpn=use_vpn)
+        converters = {
+            "timestamp": pd.to_datetime,
+            "open": self.decimal_from_value,
+            "high": self.decimal_from_value,
+            "low": self.decimal_from_value,
+            "close": self.decimal_from_value,
+        }
+        # Endpoint Configs #
+        self.logging_info_start(
+            endpoint=endpoint, intervals=possible_intervals, use_vpn=use_vpn
+        )
         call = (
             self.api_mock_call.format(
                 endpoint=endpoint, ticker=ticker, api_key=self.get_random_api_key()
@@ -31,26 +42,37 @@ class AlphaStockTimeSeries(AlphaData):
             + f"&interval={interval}&adjusted={str(adjusted).lower()}&outputsize=full&datatype=csv"
         )
         return self.response_csv_to_pandas_handling(
-            self.get_call_api(call, use_vpn=use_vpn)
+            response=self.get_call_api(call, use_vpn=use_vpn), converters=converters
         )
 
     def df_time_series_intraday_extended(
         self,
         ticker: str,
-        interval: str,
+        interval: str = "60min",
         adjusted: bool = True,
         year_range: int = 2,
         use_vpn: bool = False,
     ) -> pd.DataFrame:
+        # Endpoint Configs
         endpoint = "TIME_SERIES_INTRADAY_EXTENDED"
         possible_intervals = ["1min", "5min", "15min", "30min", "60min"]
+        converters = {
+            "time": pd.to_datetime,
+            "open": self.decimal_from_value,
+            "high": self.decimal_from_value,
+            "low": self.decimal_from_value,
+            "close": self.decimal_from_value,
+        }
+        # Endpoint Configs #
         if year_range > 2:
             logging.error(
                 'By limitations of the API, the maximum value for the param "year_range" is 2'
             )
             raise ValueError
         else:
-            self.logging_info_start(endpoint=endpoint, intervals=possible_intervals, use_vpn=use_vpn)
+            self.logging_info_start(
+                endpoint=endpoint, intervals=possible_intervals, use_vpn=use_vpn
+            )
             api_calls = [
                 self.api_mock_call.format(
                     endpoint=endpoint, ticker=ticker, api_key=self.get_random_api_key()
@@ -65,7 +87,7 @@ class AlphaStockTimeSeries(AlphaData):
             api_responses = list(self.futures_api_calls(api_calls, use_vpn))
             aggregated_df = pd.concat(
                 [
-                    self.response_csv_to_pandas_handling(response)
+                    self.response_csv_to_pandas_handling(response=response, converters=converters)
                     for response in api_responses
                 ]
             )
@@ -79,9 +101,25 @@ class AlphaStockTimeSeries(AlphaData):
     ) -> pd.DataFrame:
         if adjusted is True:
             endpoint = "TIME_SERIES_DAILY_ADJUSTED"
+            converters = {
+                "timestamp": pd.to_datetime,
+                "open": self.decimal_from_value,
+                "high": self.decimal_from_value,
+                "low": self.decimal_from_value,
+                "close": self.decimal_from_value,
+                "adjusted_close": self.decimal_from_value,
+                "dividend_amount": self.decimal_from_value,
+                "split_coefficient": self.decimal_from_value
+            }
         else:
             endpoint = "TIME_SERIES_DAILY"
-
+            converters = {
+                "timestamp": pd.to_datetime,
+                "open": self.decimal_from_value,
+                "high": self.decimal_from_value,
+                "low": self.decimal_from_value,
+                "close": self.decimal_from_value
+            }
         self.logging_info_start(endpoint=endpoint, use_vpn=use_vpn)
         call = (
             self.api_mock_call.format(
@@ -90,7 +128,7 @@ class AlphaStockTimeSeries(AlphaData):
             + f"&outputsize=full&datatype=csv"
         )
         return self.response_csv_to_pandas_handling(
-            self.get_call_api(call, use_vpn=use_vpn)
+            self.get_call_api(call, use_vpn=use_vpn), converters=converters
         )
 
     def df_time_series_weekly(
@@ -101,8 +139,24 @@ class AlphaStockTimeSeries(AlphaData):
     ) -> pd.DataFrame:
         if adjusted is True:
             endpoint = "TIME_SERIES_WEEKLY_ADJUSTED"
+            converters = {
+                "timestamp": pd.to_datetime,
+                "open": self.decimal_from_value,
+                "high": self.decimal_from_value,
+                "low": self.decimal_from_value,
+                "close": self.decimal_from_value,
+                "adjusted close": self.decimal_from_value,
+                "dividend amount": self.decimal_from_value
+            }
         else:
             endpoint = "TIME_SERIES_WEEKLY"
+            converters = {
+                "timestamp": pd.to_datetime,
+                "open": self.decimal_from_value,
+                "high": self.decimal_from_value,
+                "low": self.decimal_from_value,
+                "close": self.decimal_from_value,
+            }
 
         self.logging_info_start(endpoint=endpoint, use_vpn=use_vpn)
         call = (
@@ -112,7 +166,7 @@ class AlphaStockTimeSeries(AlphaData):
             + f"&datatype=csv"
         )
         return self.response_csv_to_pandas_handling(
-            self.get_call_api(call, use_vpn=use_vpn)
+            self.get_call_api(call, use_vpn=use_vpn), converters=converters
         )
 
     def df_time_series_monthly(
@@ -123,8 +177,25 @@ class AlphaStockTimeSeries(AlphaData):
     ) -> pd.DataFrame:
         if adjusted is True:
             endpoint = "TIME_SERIES_MONTHLY_ADJUSTED"
+            converters = {
+                "timestamp": pd.to_datetime,
+                "open": self.decimal_from_value,
+                "high": self.decimal_from_value,
+                "low": self.decimal_from_value,
+                "close": self.decimal_from_value,
+                "adjusted close": self.decimal_from_value,
+                "dividend amount": self.decimal_from_value
+            }
         else:
             endpoint = "TIME_SERIES_MONTHLY"
+            converters = {
+                "timestamp": pd.to_datetime,
+                "open": self.decimal_from_value,
+                "high": self.decimal_from_value,
+                "low": self.decimal_from_value,
+                "close": self.decimal_from_value
+            }
+
 
         self.logging_info_start(endpoint=endpoint, use_vpn=use_vpn)
         call = (
@@ -134,7 +205,7 @@ class AlphaStockTimeSeries(AlphaData):
             + f"&datatype=csv"
         )
         return self.response_csv_to_pandas_handling(
-            self.get_call_api(call, use_vpn=use_vpn)
+            self.get_call_api(call, use_vpn=use_vpn), converters=converters
         )
 
     def df_quote_endpoint(
@@ -152,5 +223,5 @@ class AlphaStockTimeSeries(AlphaData):
             + f"&datatype=csv"
         )
         return self.response_csv_to_pandas_handling(
-            self.get_call_api(call, use_vpn=use_vpn)
+            self.get_call_api(call, use_vpn=use_vpn), converters=None
         )
