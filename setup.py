cb534cc10a58730e1fe8dd487b681438a04a46fc
RicardoPedrotti <rpedrottivf@gmail.com>
2021-06-22 23:31:52 +0200

    package build tools

--- setup.py
+++ setup.py
@@ -1,3 +1,36 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name="fenix_alpha_vantage", version="0.0.1", packages=["fenix_alpha_vantage"])
+
+VERSION = '1.0.0'
+NAME = "fenix_alpha_vantage"
+DESCRIPTION = 'Proprietary Fenix package to interact with Alpha Vantage APIs.'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+# Setting up
+setup(
+    name=NAME,
+    version=VERSION,
+    author="Ricardo Pedrotti",
+    author_email="rpedrottivf@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        "pandas = ~=1.2.4",
+        "matplotlib = ~=3.4.2",
+        "scikit-learn = ~=0.24.2",
+        "seaborn = ~=0.11.1",
+        "convertdate = ~=2.3.2",
+        "pystan = ==2.18.0.0",
+        "plotly = ~=5.0.0",
+        "notebook = ~=6.4.0",
+        "pyyaml = ~=5.4.1",
+        "requests = ~=2.25.1",
+    ],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    python_requires='>=3',
+    keywords=['python', 'alpha vantage', 'fenix'],
+)
