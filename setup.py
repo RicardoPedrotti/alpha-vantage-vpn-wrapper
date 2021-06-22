2863c525dc89350b3dd8bf642b687436a7b8aa30
RicardoPedrotti <rpedrottivf@gmail.com>
2021-06-23 01:38:32 +0200

    updating version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 NAME = "fenix_alpha_vantage"
 DESCRIPTION = 'Proprietary Fenix package to interact with Alpha Vantage APIs.'
 
